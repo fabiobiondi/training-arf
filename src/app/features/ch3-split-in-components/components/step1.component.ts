84fb1d3c6ad8d8e65c1b00358891bce4d2623772
Fabio <info@fabiobiondi.com>
2019-06-11 16:57:22 +0200

    Remove FormGroup

--- src/app/features/ch3-split-in-components/components/step1.component.ts
+++ src/app/features/ch3-split-in-components/components/step1.component.ts
@@ -4,34 +4,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'fb-step-1',
   template: `
-    <div [formGroup]="group">
-
+    <div [formGroup]="group.get('step1')" *ngIf="group.get('step1') as subGroup" >
+      <h4>
+        <i class="fa fa-check" *ngIf="subGroup.valid" style="color: green"></i>
+        Step1: Car Info
+      </h4>
       
-      <div [formGroup]="subGroup"
-           *ngIf="group.get('step1') as subGroup">
+      <div class="form-group">
+        <input type="text"
+               formControlName="brand"
+               class="form-control"
+               placeholder="Brand *"
+               [ngClass]="{'is-invalid': subGroup?.controls['brand']?.invalid}"
+        >
+      </div>
 
-        <h4>
-          <i class="fa fa-check" *ngIf="subGroup.valid" style="color: green"></i>
-          Step1: Car Info
-        </h4>
-        
-        <div class="form-group">
-          <input type="text"
-                 formControlName="brand"
-                 class="form-control"
-                 placeholder="Brand *"
-                 [ngClass]="{'is-invalid': subGroup?.controls['brand']?.invalid}"
-          >
-        </div>
-
-        <div class="form-group">
-          <input type="text"
-                 formControlName="model"
-                 class="form-control"
-                 placeholder="Model *"
-                 [ngClass]="{'is-invalid': subGroup?.controls['model']?.invalid}"
-          >
-        </div>
+      <div class="form-group">
+        <input type="text"
+               formControlName="model"
+               class="form-control"
+               placeholder="Model *"
+               [ngClass]="{'is-invalid': subGroup?.controls['model']?.invalid}"
+        >
       </div>
     </div>
   `
--- src/app/features/ch3-split-in-components/components/step2.component.ts
+++ src/app/features/ch3-split-in-components/components/step2.component.ts
@@ -4,32 +4,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'fb-step-2',
   template: `
-    <div [formGroup]="group">
-      <div [formGroup]="subGroup"
-           *ngIf="group.get('step2') as subGroup">
+    <div [formGroup]="group.get('step2')" *ngIf="group.get('step2') as subGroup" >
+      <h4>
+        <i class="fa fa-check" *ngIf="subGroup.valid" style="color: green"></i>
+        Step2: Car Details
+      </h4>
+      
+      <div class="form-group">
+        <input type="text"
+               formControlName="kw"
+               class="form-control"
+               placeholder="Kw *"
+               [ngClass]="{'is-invalid': subGroup?.controls['kw']?.invalid}"
+        >
+      </div>
 
-        <h4>
-          <i class="fa fa-check" *ngIf="subGroup.valid" style="color: green"></i>
-          Step2: Car Details
-        </h4>
-        
-        <div class="form-group">
-          <input type="text"
-                 formControlName="kw"
-                 class="form-control"
-                 placeholder="Kw *"
-                 [ngClass]="{'is-invalid': subGroup?.controls['kw']?.invalid}"
-          >
-        </div>
-
-        <div class="form-group">
-          <input type="text"
-                 formControlName="km"
-                 class="form-control"
-                 placeholder="Km *"
-                 [ngClass]="{'is-invalid': subGroup?.controls['km']?.invalid}"
-          >
-        </div>
+      <div class="form-group">
+        <input type="text"
+               formControlName="km"
+               class="form-control"
+               placeholder="Km *"
+               [ngClass]="{'is-invalid': subGroup?.controls['km']?.invalid}"
+        >
       </div>
     </div>
   `
