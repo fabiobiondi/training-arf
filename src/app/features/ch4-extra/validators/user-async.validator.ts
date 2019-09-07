5c27cade376d8b064366b3f31fd45c4c2c5ef907
Fabio Biondi <info@fabiobiondi.com>
2019-09-07 02:44:07 +0200

    Clean imports

--- src/app/features/ch4-extra/validators/user-async.validator.ts
+++ src/app/features/ch4-extra/validators/user-async.validator.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AbstractControl, AsyncValidatorFn, FormControl } from '@angular/forms';
+import { AsyncValidatorFn, FormControl } from '@angular/forms';
 import { Observable, timer } from 'rxjs';
 import { map, switchMap  } from 'rxjs/operators';
 
